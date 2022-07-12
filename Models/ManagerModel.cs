8a20b50984241d7c7a4d6a53526db722c75ec2ae
AdityaBhadang <bhadangaditya@gmail.com>
2022-07-12 16:54:38 +0530

    Manager Model Added

---
+++ Models/ManagerModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectAPI.Models
+{
+    public class ManagerModel
+    {
+        public class ManagerModelDB
+        {
+            public int ManagerId { get; set; }
+            [Required]
+            public string ManagerName { get; set; }
+            [Required]
+            public string ManagerEmail { get; set; }
+            [Required]
+            public long ManagerMobileNumber { get; set; }
+        }
+    }
+}
---
+++ Models/ManagerModelDB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectAPI.Models
+{
+    public class ManagerModelDB
+    {
+        [Key]
+        public int ManagerId { get; set; }
+        public string ManagerName { get; set; }
+        public string ManagerEmail { get; set; }
+        public long ManagerMobileNumber { get; set; }
+    }
+}
