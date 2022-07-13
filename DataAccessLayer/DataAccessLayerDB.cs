696a5c5d82f423f1546aa74de2d6ac0cb0c7c174
AdityaBhadang <bhadangaditya@gmail.com>
2022-07-13 09:41:32 +0530

    accesslayerchange

--- DataAccessLayer/DataAccessLayerDB.cs
+++ DataAccessLayer/DataAccessLayerDB.cs
@@ -16,6 +16,7 @@ namespace ProjectAPI.DataAccessLayer
 
         public DbSet<EmployeeModelDB> Employee { get; set; }
         public DbSet<ManagerModelDB> Manager { get; set; }
+        public DbSet<LeaveSectionDB> LeaveSection { get; set; }
 
     }
 }
